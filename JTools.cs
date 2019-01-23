d120035f1f386179b3893c65715c1e37ecfa8aed
Jonathan <jmc@devj.uk>
2019-01-23 16:27:16 +0000

    Create JTools.cs
    Created and added a basic function to the tools where you can rename gameobjects in batch. Yes you can already do this without this tool, buts its my first function so I'm happy about it :smile:

---
+++ JTools.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class JTools : EditorWindow
+{
+
+	private string RenameString;
+	private bool ShowRenameGameObject;
+
+
+
+
+
+	[MenuItem("Window/J-Tools")]
+	public static void ShowWindow()
+	{
+		GetWindow<JTools>("J-Tools");
+	}
+
+
+	private void OnGUI()
+	{
+		GUILayout.Space(10f);
+		GUILayout.Label("J-Tools", EditorStyles.boldLabel);
+		GUILayout.Label("A random selection of tools that may or may not be useful. This is mostly for fun :)");
+
+
+
+		GUILayout.Space(10f);
+		GUILayout.Label("Rename Objects", EditorStyles.boldLabel);
+
+		ShowRenameGameObject = EditorGUILayout.Foldout(ShowRenameGameObject, "Open Options");
+
+		if (ShowRenameGameObject)
+		{
+			GUILayout.BeginHorizontal();
+
+			RenameString = EditorGUILayout.TextField("Name String Value", RenameString);
+
+			if (GUILayout.Button("Change Name"))
+			{
+				RenameObject();
+			}
+
+			GUILayout.EndHorizontal();
+		}
+
+	}
+
+
+	private void RenameObject()
+	{
+		foreach (GameObject Obj in Selection.gameObjects)
+		{
+			if (Obj.name != RenameString)
+			{
+				Obj.name = RenameString;
+			}
+		}
+	}
+}
