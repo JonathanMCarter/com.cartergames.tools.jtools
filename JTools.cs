6371c776b4f68e120c1f90917d31ac4389d1125e
Jonathan <jmc@devj.uk>
2019-03-27 18:24:24 +0000

    Auto Save Tool Added
    New auto save tool to save the scene after a defined time delay.

--- JTools.cs
+++ JTools.cs
@@ -5,6 +5,7 @@ public class JTools : EditorWindow
 {
 	/* Booleans for if the windows are open */
 	private bool IsMeshMaterialOpen;
+	private bool IsAutoSaveOpen;
 
 	/* Custom Colours */
 	private Color32 LightGreen1 = new Color32(70, 193, 64, 255);
@@ -35,6 +36,15 @@ public class JTools : EditorWindow
 	/* Function that shows the tools and the buttons to open them */
 	private void OpenWindows()
 	{
+		MeshTool();
+		GUILayout.Space(25);
+		GUI.color = Color.white;
+		AutoSaveTool();
+	}
+
+
+	private void MeshTool()
+	{
 		EditorGUILayout.LabelField("Mesh Material Apply Tool   (Shift-M)", EditorStyles.boldLabel);
 		GUILayout.Space(10f);
 		EditorGUILayout.HelpBox("A tool to apply materials to multiple copies of the same mesh at once.", MessageType.None);
@@ -58,6 +68,40 @@ public class JTools : EditorWindow
 			{
 				GetWindow<MeshMaterialTool>().Close();
 				IsMeshMaterialOpen = false;
+			}
+		}
+
+		GUILayout.EndHorizontal();
+	}
+
+
+
+	private void AutoSaveTool()
+	{
+		EditorGUILayout.LabelField("Auto Save Scene Tool   (Shift-A)", EditorStyles.boldLabel);
+		GUILayout.Space(10f);
+		EditorGUILayout.HelpBox("A tool to auto save the open scene, incase you forget", MessageType.None);
+		EditorGUILayout.HelpBox("This tool has to be docked to work in the background", MessageType.Warning);
+		GUILayout.Space(2.5f);
+
+		GUILayout.BeginHorizontal();
+
+		GUI.color = LightGreen1;
+
+		if (GUILayout.Button("Open Tool", GUILayout.MaxWidth(100)))
+		{
+			GetWindow<AutoSaveSceneTool>();
+			IsAutoSaveOpen = true;
+		}
+
+		GUI.color = LightRed1;
+
+		if (GUILayout.Button("Close Tool", GUILayout.MaxWidth(100)))
+		{
+			if (IsAutoSaveOpen)
+			{
+				GetWindow<AutoSaveSceneTool>().Close();
+				IsAutoSaveOpen = false;
 			}
 		}
 
